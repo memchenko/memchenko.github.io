a19cbbc46ae13720093b4cd1b11b91f9418fdbdc
Mikhail Emchenko <emchenko.mikhail@mail.ru>
2024-02-12 23:21:07 +0100

    +

--- app/showcase/basic3d/script.js
+++ app/showcase/basic3d/script.js
@@ -20,7 +20,9 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
+const matcapTexture = textureLoader.load(
+  location.origin + "/textures/matcaps/8.png"
+);
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
@@ -28,43 +30,46 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace;
  */
 const fontLoader = new FontLoader();
 
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  // Material
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+fontLoader.load(
+  location.origin + "/fonts/helvetiker_regular.typeface.json",
+  (font) => {
+    // Material
+    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
-  // Text
-  const textGeometry = new TextGeometry("Hello Three.js", {
-    font: font,
-    size: 0.5,
-    height: 0.2,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 0.03,
-    bevelSize: 0.02,
-    bevelOffset: 0,
-    bevelSegments: 5,
-  });
-  textGeometry.center();
+    // Text
+    const textGeometry = new TextGeometry("Hello Three.js", {
+      font: font,
+      size: 0.5,
+      height: 0.2,
+      curveSegments: 12,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 5,
+    });
+    textGeometry.center();
 
-  const text = new THREE.Mesh(textGeometry, material);
-  scene.add(text);
+    const text = new THREE.Mesh(textGeometry, material);
+    scene.add(text);
 
-  // Donuts
-  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+    // Donuts
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
 
-  for (let i = 0; i < 100; i++) {
-    const donut = new THREE.Mesh(donutGeometry, material);
-    donut.position.x = (Math.random() - 0.5) * 10;
-    donut.position.y = (Math.random() - 0.5) * 10;
-    donut.position.z = (Math.random() - 0.5) * 10;
-    donut.rotation.x = Math.random() * Math.PI;
-    donut.rotation.y = Math.random() * Math.PI;
-    const scale = Math.random();
-    donut.scale.set(scale, scale, scale);
+    for (let i = 0; i < 100; i++) {
+      const donut = new THREE.Mesh(donutGeometry, material);
+      donut.position.x = (Math.random() - 0.5) * 10;
+      donut.position.y = (Math.random() - 0.5) * 10;
+      donut.position.z = (Math.random() - 0.5) * 10;
+      donut.rotation.x = Math.random() * Math.PI;
+      donut.rotation.y = Math.random() * Math.PI;
+      const scale = Math.random();
+      donut.scale.set(scale, scale, scale);
 
-    scene.add(donut);
+      scene.add(donut);
+    }
   }
-});
+);
 
 /**
  * Sizes
